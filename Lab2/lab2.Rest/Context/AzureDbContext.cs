e73d110d08f0bb1e11b728c097ef4dafd1eae7ff
Krzysztof Musiał <mail@krzysztofmusial.com>
2020-11-13 17:20:03 +0100

    added PeopleController

---
+++ Lab2/lab2.Rest/Context/AzureDbContext.cs
@@ -0,0 +1,21 @@
+﻿using lab2.Rest.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2.Rest.Context
+{
+    class AzureDbContext : DbContext
+    {
+        public AzureDbContext(DbContextOptions<AzureDbContext> options) : base(options)
+        {
+
+        }
+        protected AzureDbContext()
+        {
+
+        }
+        public DbSet<Person> People { get; set; }
+    }
+}
---
+++ Lab2/lab2.Rest/Controllers/PeopleController.cs
@@ -0,0 +1,26 @@
+﻿using lab2.Rest.Context;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2.Rest.Controllers
+{
+    [ApiController]
+    [Route("[api/people]")]
+    class PeopleController : ControllerBase
+    {
+        private readonly AzureDbContext db;
+        public PeopleController(AzureDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(db.People);
+        }
+
+    }
+}
